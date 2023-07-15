f8a123c17eafea69a4c2f48c81c3815bfd99ce5a
Reshma Raj <reshma.raj@speehive.com>
2023-07-15 13:56:51 +0530

    Static Modifier

--- BroCode/Program.cs
+++ BroCode/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+#region Static
+//STATIC - modifier used to declare static member, which belongs to the class itself rather than to any specific objects 
+using BroCode;
+
+Car car1= new Car("Mustang");
+Car car2 = new Car("Corvette");
+//Console.WriteLine(car1.numberOfCars);
+//Console.WriteLine(car2.numberOfCars);
+Console.WriteLine(Car.numberOfCars);
+Car.StartRace();
+//eg: Math class;;;;;
+#endregion
---
+++ BroCode/StaticModifier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BroCode
+{
+    public class Car
+    {
+        public static int numberOfCars;
+        string model;
+        public Car(string model)
+        {
+            this.model = model;
+            numberOfCars++;
+        }
+        public static void StartRace()
+        {
+            Console.WriteLine("The Race starts now");
+        }
+    }
+}
